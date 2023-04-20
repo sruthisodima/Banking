adf8e87bdeb18ed903d74863ec7a21463f7c5961
sruthisodima <55868903+sruthisodima@users.noreply.github.com>
2023-04-20 23:27:20 +0530

    Update main.tf

--- project-02-bank-finacial-master/test-server/main.tf
+++ project-02-bank-finacial-master/test-server/main.tf
@@ -5,12 +5,12 @@ resource "aws_eip_association" "eip_assoc" {
 resource "aws_instance" "test-server" {
   ami           = "ami-00c39f71452c08778" 
   instance_type = "t2.micro" 
-  key_name = "DEMOKEY"
+  key_name = "jenkinskey"
   vpc_security_group_ids= ["sg-0c7aae9017fc5106b"]
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("./DEMOKEY.pem")
+    private_key = file("./jenkinskey.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
